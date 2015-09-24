2029547c418794cc2d5a46fc48f3ed78ca6f9b00
EgorSemenov <airbander@mail.ru>
2015-09-24 15:21:33 +0300

    First done

--- 01_HW.rb
+++
@@ -1,5 +0,0 @@
-puts "What is your name?"
-name = gets
-puts "What is your hight?"
-u_hight = gets.to_i
-puts (u_hight - 110) * 1.15
---
+++ hw_02/task_01.rb
@@ -0,0 +1,7 @@
+puts "put number"
+a = gets.chomp.to_i
+if a % 2 == 0
+	puts "number is integer"
+else
+	puts "number is not integer"
+end
