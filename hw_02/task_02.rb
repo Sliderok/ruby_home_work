b647734a20d80cbe200b01db9df6f0b7101c5059
EgorSemenov <airbander@mail.ru>
2015-09-24 15:29:01 +0300

    second done

---
+++ hw_02/task_02.rb
@@ -0,0 +1,18 @@
+puts "put w_num"
+w_num = gets.chomp.to_i
+case w_num
+when 1
+	puts "Mon"
+when 2
+	puts "Tue"
+when 3
+	puts "Wed"
+when 4
+	puts "Thu"
+when 5
+	puts "Fri"
+when 6
+	puts "Sat"
+when 7
+	puts "Sun"
+end
